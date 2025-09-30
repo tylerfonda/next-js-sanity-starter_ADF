4c01dc661e48333beb27af9a667dede8fc362292
Tyler Fonda <tyler@adreamfactory.studio>
2025-09-30 15:51:29 -0400

    Update artifact.ts

--- sanity/schemas/documents/artifact.ts
+++ sanity/schemas/documents/artifact.ts
@@ -1,21 +1,32 @@
-defineField({
-  name: "sources",
-  title: "Sources",
-  type: "array",
-  of: [{type: "reference", to: [{type: "source"}]}],
-}),
-defineField({
-  name: "updatedAt",
-  title: "Updated At",
-  type: "datetime",
-  initialValue: () => new Date().toISOString(),
-}),
-defineField({
-  name: "metrics",
-  title: "Metrics",
-  type: "object",
+export default defineType({
+  name: "artifact",
+  title: "Artifact",
+  type: "document",  
   fields: [
-    defineField({name: "stat", title: "Stat", type: "number"}),
-    defineField({name: "unit", title: "Unit", type: "string"}),
-  ],
-}),
+  defineField({name: "title", type: "string", validation: r=>r.required()}),
+  defineField({name: "slug", type: "slug", options: {source: "title"}}),
+  defineField({name: "body", type: "array", of: [{type: "block"}]}),
+
+  defineField({
+    name: "sources",
+    title: "Sources",
+    type: "array",
+    of: [{type: "reference", to: [{type: "source"}]}],
+  }),
+  defineField({
+    name: "updatedAt",
+    title: "Updated At",
+    type: "datetime",
+    initialValue: () => new Date().toISOString(),
+  }),
+  defineField({
+    name: "metrics",
+    title: "Metrics",
+    type: "object",
+    fields: [
+      defineField({name: "stat", title: "Stat", type: "number"}),
+      defineField({name: "unit", title: "Unit", type: "string"}),
+    ],
+  }),
+]
+
