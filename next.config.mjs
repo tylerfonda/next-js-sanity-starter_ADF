f713124614532b06deaab06afef8e4071f856cc4
Tyler Fonda <tyler@adreamfactory.studio>
2025-09-30 15:44:23 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,7 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/index',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
-  images: {
-    unoptimized: true,
-    qualities: [100],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-    ],
-  },
+  compiler: { styledComponents: true },
+  // other options...
 };
 
 export default nextConfig;
