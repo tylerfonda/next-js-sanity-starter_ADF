970604ec0027bec1ac1f14f1a4f792a8b3bc2f85
Tyler Fonda <tyler@adreamfactory.studio>
2025-09-30 15:46:05 -0400

    Create source.ts

---
+++ sanity/schemas/documents/source.ts
@@ -0,0 +1,15 @@
+import {defineType, defineField} from "sanity";
+
+export default defineType({
+  name: "source",
+  title: "Source",
+  type: "document",
+  fields: [
+    defineField({name: "title", type: "string", validation: (r)=>r.required()}),
+    defineField({name: "url", type: "url"}),
+    defineField({name: "author", type: "string"}),
+    defineField({name: "publisher", type: "string"}),
+    defineField({name: "date", type: "datetime"}),
+    defineField({name: "citation", type: "text"}),
+  ],
+});
