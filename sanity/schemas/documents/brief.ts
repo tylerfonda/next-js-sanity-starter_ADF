fa0cb4d96e2bd88823c997850bde32d5b4364ff0
Tyler Fonda <tyler@adreamfactory.studio>
2025-10-01 12:42:12 -0400

    Update brief.ts

--- sanity/schemas/documents/brief.ts
+++ sanity/schemas/documents/brief.ts
@@ -1,12 +1,25 @@
-defineField({
-  name: "sources",
-  title: "Sources",
-  type: "array",
-  of: [{type: "reference", to: [{type: "source"}]}],
-}),
-defineField({
-  name: "updatedAt",
-  title: "Updated At",
-  type: "datetime",
-  initialValue: () => new Date().toISOString(),
-}),
+import {defineType, defineField} from 'sanity'
+
+export default defineType({
+  name: 'brief',
+  title: 'Brief',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'dueAt',
+      title: 'Due At',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'text',
+    }),
+  ],
+})
